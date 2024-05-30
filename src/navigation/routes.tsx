effda7df66eb61622d0c134354c0245490c77805
Pham Thao <thaopv@Phams-MacBook-Pro.local>
2024-05-30 15:00:39 +0700

    Update dev source

---
+++ src/navigation/routes.tsx
@@ -0,0 +1,20 @@
+import type { ComponentType, JSX } from 'react';
+
+import { IndexPage } from '@/pages/IndexPage/IndexPage';
+import { InitDataPage } from '@/pages/InitDataPage/InitDataPage';
+import { LaunchParamsPage } from '@/pages/LaunchParamsPage/LaunchParamsPage.tsx';
+import { ThemeParamsPage } from '@/pages/ThemeParamsPage/ThemeParamsPage.tsx';
+
+interface Route {
+  path: string;
+  Component: ComponentType;
+  title?: string;
+  icon?: JSX.Element;
+}
+
+export const routes: Route[] = [
+  { path: '/', Component: IndexPage },
+  { path: '/init-data', Component: InitDataPage, title: 'Init Data' },
+  { path: '/theme-params', Component: ThemeParamsPage, title: 'Theme Params' },
+  { path: '/launch-params', Component: LaunchParamsPage, title: 'Launch Params' },
+];

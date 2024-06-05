7bf9e2f445fa6aba35f4fb507bb3676e7e7c7c28
ntdat1306 <ntdat1306@gmail.com>
2024-06-05 15:31:52 +0700

    pag: wallet

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -1,24 +1,24 @@
-import type { ComponentType, JSX } from "react";
+import type { ComponentType, JSX } from 'react';
 
-import { IndexPage } from "@/pages/IndexPage/IndexPage";
-import { InitDataPage } from "@/pages/InitDataPage/InitDataPage";
-import { LaunchParamsPage } from "@/pages/LaunchParamsPage/LaunchParamsPage.tsx";
-import { ThemeParamsPage } from "@/pages/ThemeParamsPage/ThemeParamsPage.tsx";
+import { IndexPage } from '@/pages/IndexPage/IndexPage';
+import { LaunchParamsPage } from '@/pages/LaunchParamsPage/LaunchParamsPage.tsx';
+import { ThemeParamsPage } from '@/pages/ThemeParamsPage/ThemeParamsPage.tsx';
+import { WalletPage } from '@/pages/WalletPage';
 
 export interface Route {
-  path: string;
-  Component: ComponentType;
-  title?: string;
-  icon?: JSX.Element;
+    path: string;
+    Component: ComponentType;
+    title?: string;
+    icon?: JSX.Element;
 }
 
 export const routes: Route[] = [
-  { path: "/", Component: IndexPage, title: "Home" },
-  { path: "/init-data", Component: InitDataPage, title: "Init Data" },
-  { path: "/theme-params", Component: ThemeParamsPage, title: "Theme" },
-  {
-    path: "/launch-params",
-    Component: LaunchParamsPage,
-    title: "Launch",
-  },
+    { path: '/', Component: IndexPage, title: 'Home' },
+    { path: '/wallet', Component: WalletPage, title: 'Wallet' },
+    { path: '/theme-params', Component: ThemeParamsPage, title: 'Theme' },
+    {
+        path: '/launch-params',
+        Component: LaunchParamsPage,
+        title: 'Launch',
+    },
 ];

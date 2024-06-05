39b9de630d398bd844dafddd207dddb71991f9e8
Pham Thao <thaopv@Phams-MacBook-Pro.local>
2024-06-05 10:51:00 +0700

    View port

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
 });

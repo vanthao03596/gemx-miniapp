4aa8b55edfbdd0b66b74ed2b1fa712153346b1b0
hungsieuhay <hungng1810@gmail.com>
2024-06-08 15:53:17 +0700

    fix route

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,84 +1,97 @@
-import { CustomHeader } from '@/components/CustomHeader';
-import useAxiosAuth from '@/hooks/useAxiosAuth';
-import { Icon24Close } from '@/icon/icon';
-import { useMutation } from '@tanstack/react-query';
-import { Button, Input, List, Tappable, Text } from '@telegram-apps/telegram-ui';
-import { AxiosError } from 'axios';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styles from './RegisterPage.module.scss';
+import { CustomHeader } from "@/components/CustomHeader";
+import useAxiosAuth from "@/hooks/useAxiosAuth";
+import { Icon24Close } from "@/icon/icon";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  Button,
+  Input,
+  List,
+  Tappable,
+  Text,
+} from "@telegram-apps/telegram-ui";
+import { AxiosError } from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./RegisterPage.module.scss";
 
 const RegisterPage = () => {
-    const navigate = useNavigate();
-    const axiosAuth = useAxiosAuth();
+  const navigate = useNavigate();
+  const axiosAuth = useAxiosAuth();
 
-    const [address, setAddress] = useState<string>('');
-    const [error, setError] = useState<string>('');
+  const [address, setAddress] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
-    const updateUser = async (address: string) => {
-        const res = await axiosAuth.post('/user/info', { address });
-        return res.data;
-    };
+  const updateUser = async (address: string) => {
+    const res = await axiosAuth.post("/user/info", { address });
+    return res.data;
+  };
 
-    const mutation = useMutation({
-        mutationKey: ['update-user'],
-        mutationFn: updateUser,
-        onSuccess: () => {
-            navigate('/');
-        },
-        onError: (error) => {
-            if (error instanceof AxiosError) {
-                setError(error.response?.data.message);
-            }
-        },
-    });
+  const queyrClient = useQueryClient();
 
-    const handleUpdateUser = () => {
-        mutation.mutate(address);
-    };
+  const mutation = useMutation({
+    mutationKey: ["update-user"],
+    mutationFn: updateUser,
+    onSuccess: async () => {
+      await queyrClient.invalidateQueries({ queryKey: ["get-user"] });
+      navigate("/");
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        setError(error.response?.data.message);
+      }
+    },
+  });
 
-    const handleChangeAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setAddress(e.target.value);
-        setError('');
-    };
+  const handleUpdateUser = () => {
+    mutation.mutate(address);
+  };
 
-    const handleClearAddress = () => {
-        setAddress('');
-        setError('');
-    };
+  const handleChangeAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddress(e.target.value);
+    setError("");
+  };
 
-    return (
-        <div className={styles.container}>
-            <CustomHeader title="Let's start" />
-            <List>
-                <Input
-                    status={error ? 'error' : 'focused'}
-                    header='Wallet Address'
-                    placeholder={error ? error : '0x . . .'}
-                    value={address}
-                    onChange={handleChangeAddress}
-                    after={
-                        <Tappable
-                            Component='div'
-                            style={{
-                                display: 'flex',
-                            }}
-                            onClick={handleClearAddress}
-                        >
-                            <Icon24Close />
-                        </Tappable>
-                    }
-                />
-                {error && <Text className={styles.error}>{error}</Text>}
-            </List>
+  const handleClearAddress = () => {
+    setAddress("");
+    setError("");
+  };
 
-            <div className={`${styles.submit} ${styles.center}`}>
-                <Button loading={mutation.isPending} disabled={!address} onClick={handleUpdateUser}>
-                    Continue
-                </Button>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <CustomHeader title="Let's start" />
+      <List>
+        <Input
+          status={error ? "error" : "focused"}
+          header="Wallet Address"
+          placeholder={error ? error : "0x . . ."}
+          value={address}
+          onChange={handleChangeAddress}
+          after={
+            <Tappable
+              Component="div"
+              style={{
+                display: "flex",
+              }}
+              onClick={handleClearAddress}
+            >
+              <Icon24Close />
+            </Tappable>
+          }
+        />
+        {error && <Text className={styles.error}>{error}</Text>}
+      </List>
+
+      <div className={`${styles.submit} ${styles.center}`}>
+        <Button
+          loading={mutation.isPending}
+          disabled={!address}
+          onClick={handleUpdateUser}
+        >
+          Continue
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;

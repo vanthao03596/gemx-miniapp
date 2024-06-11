ff269da3a97d897d7970cba4ab2053d8d1ba7c81
Pham Thao <thaopv@Phams-MacBook-Pro.local>
2024-06-11 14:55:03 +0700

    Fix link

--- src/context/AxiosProvider.tsx
+++ src/context/AxiosProvider.tsx
@@ -1,4 +1,5 @@
 import { axiosAuth } from "@/lib/axios";
+import { useLaunchParams } from "@tma.js/sdk-react";
 import { AxiosInstance } from "axios";
 import React, { createContext, useLayoutEffect } from "react";
 
@@ -14,9 +15,10 @@ export const AxiosContext = createContext<AxiosContextType>(null!);
 
 const AxiosProvider = (props: AxiosProviderProps) => {
   const { children } = props;
-  // const initData = useLaunchParams().initDataRaw;
-  const initData =
-      'query_id=AAHwSlYxAAAAAPBKVjGF9bzY&user=%7B%22id%22%3A827738864%2C%22first_name%22%3A%22Ph%E1%BA%A1m%22%2C%22last_name%22%3A%22Th%E1%BA%A3o%22%2C%22username%22%3A%22phamthao03596%22%2C%22language_code%22%3A%22vi%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1717838295&hash=5329fd4df3adcd99edf8734516f8981cc8aea7c83c427284cfb93e8b2c29d1e9';
+  const initData = useLaunchParams().initDataRaw;
+  console.log(initData);
+  // const initData =
+  //     'query_id=AAHwSlYxAAAAAPBKVjGF9bzY&user=%7B%22id%22%3A827738864%2C%22first_name%22%3A%22Ph%E1%BA%A1m%22%2C%22last_name%22%3A%22Th%E1%BA%A3o%22%2C%22username%22%3A%22phamthao03596%22%2C%22language_code%22%3A%22vi%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1717838295&hash=5329fd4df3adcd99edf8734516f8981cc8aea7c83c427284cfb93e8b2c29d1e9';
 
   useLayoutEffect(() => {
     const requestIntercept = axiosAuth.interceptors.request.use((config) => {
